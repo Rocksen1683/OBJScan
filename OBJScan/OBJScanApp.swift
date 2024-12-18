1d4755a9a5822cefb43b9f7293f51a1b868d14f3
Rocksen1683 <edpravee@uwaterloo.ca>
2024-12-18 13:36:47 -0500

    Initial Commit

---
+++ OBJScan/OBJScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  OBJScanApp.swift
+//  OBJScan
+//
+//  Created by Ekanshh Praveen on 2024-12-18.
+//
+
+import SwiftUI
+
+@main
+struct OBJScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OBJScanTests/OBJScanTests.swift
@@ -0,0 +1,17 @@
+//
+//  OBJScanTests.swift
+//  OBJScanTests
+//
+//  Created by Ekanshh Praveen on 2024-12-18.
+//
+
+import Testing
+@testable import OBJScan
+
+struct OBJScanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
